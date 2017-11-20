27aedb2b4308129d07bb46d879a9e4fd03f7067c
Henry Alpert <halpert3@gmail.com>
2017-11-20 11:14:07 -0600

    Add root

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   resources :dogs
   resources :vets
   resources :owners
+
+  root 'pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

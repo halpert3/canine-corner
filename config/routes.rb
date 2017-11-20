daec3ea4ea3660fb5c99e4af5c62691563980cfd
Henry Alpert <halpert3@gmail.com>
2017-11-20 11:12:00 -0600

    Add breed seet. Add static pages. Set root.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'pages/index'
+
+  get 'pages/about'
+
   resources :dogs
   resources :vets
   resources :owners
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+breed_names = [
+  'German Shepherd',
+  'Bulldog',
+  'Catahoula',
+  'Dalmation',
+  'Labrador Retriever',
+  'Beagle',
+  'Basset Hound',
+  'Poodle',
+  'Maltese',
+  'Mutt',
+  'Jack Russell Terrier',
+  'Pit Bull',
+  'Chihuahua',
+  'Golden Retriever',
+  'Terrier',
+  'Shi-Tzu',
+  'Chow',
+  'Greyhound',
+  'Huskie',
+  'Great Dane'
+]
+
+breed_names.each do |x|
+  Breed.create(name: x)
+end
+
+puts "Breeds seeded!"
+
+

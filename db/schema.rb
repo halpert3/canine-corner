a99a05dd12e3450af749d8c628aabd5c5ba3a7a1
Henry Alpert <halpert3@gmail.com>
2017-11-20 10:29:17 -0600

    Add breed model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171120162121) do
+ActiveRecord::Schema.define(version: 20171120162716) do
+
+  create_table "breeds", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "owners", force: :cascade do |t|
     t.string "first_name"

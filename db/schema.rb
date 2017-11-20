b09dbdd3ae78c3e2c1faa7353d3a9ab5f141ea53
Henry Alpert <halpert3@gmail.com>
2017-11-20 10:33:52 -0600

    Add vets

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171120162716) do
+ActiveRecord::Schema.define(version: 20171120163134) do
 
   create_table "breeds", force: :cascade do |t|
     t.string "name"
@@ -29,6 +29,15 @@ ActiveRecord::Schema.define(version: 20171120162716) do
     t.string "secondary_phone"
     t.string "emergency_name"
     t.string "emergency_phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "vets", force: :cascade do |t|
+    t.string "doctor"
+    t.string "clinic"
+    t.string "phone"
+    t.string "fax"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
